94d8f5b32c738f35623eb59ebe7f29a5890ca223
afluex-sudhakar <developer2.afluex@gmail.com>
2022-03-07 18:21:47 +0530

    changes

--- MyTrade/Controllers/UserController.cs
+++ MyTrade/Controllers/UserController.cs
@@ -62,6 +62,29 @@ namespace MyTrade.Controllers
                 }
                 ViewBag.Tr2Business = ds.Tables[1].Rows[0]["Tr2Business"].ToString();
             }
+
+            
+         
+            List<Dashboard> lst = new List<Dashboard>();
+            obj.AddedBy = Session["Pk_userId"].ToString();
+            DataSet ds1 = obj.GetRewarDetails();
+            if (ds1 != null && ds1.Tables.Count > 0 && ds1.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow r in ds1.Tables[0].Rows)
+                {
+                    Dashboard obj1 = new Dashboard();
+                    obj1.PK_RewardId = r["PK_RewardId"].ToString();
+                    obj1.Title = r["Title"].ToString();
+                    obj1.Image = "/UploadReward/" + r["postedFile"].ToString();
+                    lst.Add(obj1);
+                }
+                obj.lstReward = lst;
+            }
+
+
+
+
+
             return View(obj);
         }
         public ActionResult ActivateByPin(User model)
@@ -1108,7 +1131,6 @@ namespace MyTrade.Controllers
                     lst.Add(obj);
                 }
                 model.lstBReports = lst;
-
                 ViewBag.Amount = double.Parse(ds.Tables[0].Compute("sum(Amount)", "").ToString()).ToString("n2");
                 ViewBag.BV = double.Parse(ds.Tables[0].Compute("sum(BV)", "").ToString()).ToString("n2");
             }
@@ -1172,8 +1194,10 @@ namespace MyTrade.Controllers
                     lst.Add(obj);
                 }
                 model.lstBReports = lst;
+
                 ViewBag.Amount = double.Parse(ds.Tables[0].Compute("sum(Amount)", "").ToString()).ToString("n2");
                 ViewBag.BV = double.Parse(ds.Tables[0].Compute("sum(BV)", "").ToString()).ToString("n2");
+
             }
 
             #region ddlPlotSize
@@ -1227,6 +1251,7 @@ namespace MyTrade.Controllers
                     obj.LoginId = r["LoginId"].ToString();
                     obj.Name = r["Name"].ToString();
                     obj.ROIPercentage = r["BackColor"].ToString();
+                    obj.TransactionNo = r["TransactionNo"].ToString();
                     lst.Add(obj);
                 }
                 model.lstPayoutRequest = lst;
@@ -1273,5 +1298,52 @@ namespace MyTrade.Controllers
             }
             return RedirectToAction("PayoutRequest", "User");
         }
+
+
+        public ActionResult Download()
+        {
+            User model = new User();
+            List<User> lst = new List<User>();
+            model.AddedBy = Session["Pk_userId"].ToString();
+            DataSet ds = model.GetRewarDetails();
+            if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow r in ds.Tables[0].Rows)
+                {
+                    User obj = new User();
+                    obj.PK_RewardId = r["PK_RewardId"].ToString();
+                    obj.Title = r["Title"].ToString();
+                    obj.Image = "/UploadReward/" + r["postedFile"].ToString();
+                    lst.Add(obj);
+                }
+                model.lstReward = lst;
+            }
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ActionName("Download")]
+        public ActionResult Download(User model)
+        {
+            List<User> lst = new List<User>();
+             model.AddedBy = Session["Pk_userId"].ToString();
+            DataSet ds = model.GetRewarDetails();
+            if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow r in ds.Tables[0].Rows)
+                {
+                    User obj = new User();
+                    obj.PK_RewardId = r["PK_RewardId"].ToString();
+                    obj.Title = r["Title"].ToString();
+                    obj.Image = "/UploadReward/" + r["postedFile"].ToString();
+                    lst.Add(obj);
+                }
+                model.lstReward = lst;
+            }
+            return View(model);
+        }
+
+
     }
 }
--- MyTrade/Models/Dashboard.cs
+++ MyTrade/Models/Dashboard.cs
@@ -17,6 +17,14 @@ namespace MyTrade.Models
         public string MessageTitle { get;  set; }
         public string Pk_MessageId { get;  set; }
 
+
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public List<Dashboard> lstReward { get; set; }
+        public string PK_RewardId { get; set; }
+
+
+
         public DataSet GetAssociateDashboard()
         {
             SqlParameter[] para = { new SqlParameter("@Fk_UserId", FK_UserId), };
@@ -27,6 +35,16 @@ namespace MyTrade.Models
         {
 
             DataSet ds = DBHelper.ExecuteQuery("GetDashBoardDetails");
+            return ds;
+        }
+
+
+        public DataSet GetRewarDetails()
+        {
+            SqlParameter[] para = {
+                new SqlParameter("@Title",Title)
+            };
+            DataSet ds = DBHelper.ExecuteQuery("GetRewarDetails", para);
             return ds;
         }
     }
