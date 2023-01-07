55d6ed0244cfd5a8f18dcae44240b251a874958b
Sudhakar Sharma <developer2.afluex@gmail.com>
2023-01-07 10:45:57 +0530

    changes

---
+++ MyTrade/obj/Release/Package/PackageTmp/MyTradeWebsite/assets/js/header.js
@@ -0,0 +1,74 @@
+document.write(`
+<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="keywords" content="">		
+		<!-- SITE TITLE -->
+		<title>My Trade</title>			
+		<!-- Latest Bootstrap min CSS -->
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">		
+		<!-- Google Font -->
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">  
+		<link rel="icon" type="image/png" href="assets/img/favicon.png">
+		<!-- Font Awesome CSS -->
+		<link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/fonts/themify-icons.css">
+		<!--- owl carousel Css-->
+		<link rel="stylesheet" href="assets/owlcarousel/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/owlcarousel/css/owl.theme.css">
+		<!--slicknav Css-->
+        <link rel="stylesheet" href="assets/css/slicknav.css">		
+		<!-- animate CSS -->
+		<link rel="stylesheet" href="assets/css/animate.css">	
+		<!-- Style CSS -->		
+		<link rel="stylesheet" href="assets/css/style.css">			
+		<link rel="stylesheet" href="assets/css/responsive.css">
+
+        <div class="body-particles"></div>
+
+        <!-- particles.js container -->
+        <div id="particles-js"></div>	
+		<!-- START PRELOADER -->
+		<div class="preloader">
+			<div class="status">
+				<div class="status-mes"></div>
+			</div>
+		</div>
+		<!-- END PRELOADER -->		
+
+		<!-- START NAVBAR -->  
+		<div id="navigation" class="fixed-top navbar-light bg-faded site-navigation">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-4">
+						<div class="site-logo">
+							<a class="navbar-logo" href="index.html"><img src="assets/img/logo-2.png" alt=""></a>
+						</div>
+					</div><!--- END Col -->					
+					<div class="col-lg-9 col-md-9 col-sm-8">
+						<div class="header_right">
+							<nav id="main-menu" class="ml-auto">
+								<ul>
+								<li><a href="index.html">Home</a></li>
+								<li><a href="about.html">About</a></li>
+								<li><a href="index.html#token_sale">Token sale</a></li>
+								<li><a href="roadmap.html">Roadmap</a></li>								  							  
+								<li><a href="faq.html">faq</a></li>	
+								<li><a href="index.html#download">Get App</a></li>									  
+								<li><a href="contact.html">Contact Us</a></li>
+								<li><a class ="block-menu" href="/Home/Login">Login</a></li>
+								</ul>
+							</nav>
+							<div id="mobile_menu"></div>
+						</div>
+					</div><!--- END Col -->
+				</div><!--- END ROW -->
+			</div><!--- END CONTAINER -->
+		</div> 	  
+		<!-- END NAVBAR -->
+
+
+`);
