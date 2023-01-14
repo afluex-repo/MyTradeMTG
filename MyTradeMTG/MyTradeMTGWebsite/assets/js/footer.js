546a9c319a4e7ae66c20db495a14ce783e2db596
unknown <designer.afluex@gmail.com>
2023-01-14 16:32:45 +0530

    changes in website

--- MyTradeMTG/MyTradeMTGWebsite/assets/js/footer.js
+++ MyTradeMTG/MyTradeMTGWebsite/assets/js/footer.js
@@ -4,9 +4,10 @@ document.write(`
 		<div class="footer">
 			<div class="container">		
 				<div class="row text-center">						
-					<div class="col-lg-12 col-sm-12 col-xs-12">
+					<div class ="col-lg-12 col-sm-12 col-xs-12">
 						<div class="footer_profile">
-						<img src="assets/img/footer_logo.png" class ="img-fluid mb-4" style="height:100px;" />
+						<img src="/UserPanel/images/MtgCoinPng.png" class ="img-fluid mb-1" style="height:5rem;" />
+                        <h2 class="text-dark mb-4 mtg_footer">MTG Coin</h2>
 							<ul>
 								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
 								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
@@ -23,34 +24,54 @@ document.write(`
 			</div><!--- END CONTAINER -->
 		</div>
 		<!-- END FOOTER -->	
-
-<!-- Latest jQuery -->
-			<script src="assets/js/jquery-1.12.4.min.js"></script>
-		<!-- Latest compiled and minified Bootstrap -->
-			<script src="assets/bootstrap/js/bootstrap.min.js"></script>					
-		<!-- owl-carousel min js  -->
+        			<script src="assets/js/jquery-1.12.4.min.js"></script>
+		<!--Latest compiled and minified Bootstrap-->
+			<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+		<!--owl-carousel min js-->
 			<script src="assets/owlcarousel/js/owl.carousel.min.js"></script>
-		<!-- jquery counterup -->
-			<script src="assets/js/jquery.counterup.min.js"></script>	
-			<script src="assets/js/countdown.js"></script>	
-		<!-- jquery.slicknav -->
-			<script src="assets/js/jquery.slicknav.js"></script>	
-		<!-- particles -->
+		<!--jquery counterup-->
+			<script src="assets/js/jquery.counterup.min.js"></script>
+			<script src="assets/js/countdown.js"></script>
+		<!--jquery.slicknav-->
+			<script src="assets/js/jquery.slicknav.js"></script>
+		<!--particles-->
 			<script src="assets/js/particles.min.js"></script>
-			<script src="assets/js/app.js"></script>			
-		<!-- jquery.smooth-scroll -->
-			<script src="assets/js/smooth-scroll.js"></script>			
-		<!-- magnific-popup js -->               
-			<script src="assets/js/jquery.magnific-popup.min.js"></script>				
-		<!-- scrolltopcontrol js -->
-			<script src="assets/js/scrolltopcontrol.js"></script>	
-		<!-- WOW - Reveal Animations When You Scroll -->
-			<script src="assets/js/wow.min.js"></script>		
-		
-		<!-- scripts js -->
+			<script src="assets/js/app.js"></script>
+		<!--jquery.smooth-scroll-->
+			<script src="assets/js/smooth-scroll.js"></script>
+		<!--magnific-popup js-->
+			<script src="assets/js/jquery.magnific-popup.min.js"></script>
+		<!--scrolltopcontrol js-->
+			<script src="assets/js/scrolltopcontrol.js"></script>
+		<!--WOW -Reveal Animations When You Scroll-->
+			<script src="assets/js/wow.min.js"></script>
+
+		<!--scripts js-->
 			<script src="assets/js/main.js"></script>
 			<script src="assets/js/trading-widget.min.js"></script>
 			<script src="assets/js/utilities.min.js"></script>
             <script src="assets/js/config-theme.js"></script>
+		<!--jquery leanModal min js-->
+			<script src="assets/js/jquery.leanModal.min.js"></script>
+		<!--login js-->
+			<script src="assets/js/login.js"></script>
+		<!--scripts js-->
+			<script src="assets/js/main.js"></script>
+            <script>
+            $('#modal').hide();
+            $('#modal_trigger').click(function()
+                {
+            $('#modal').toggle();
+             })
+             $('#modal_sign').click(function()
+            {         
+                 $('#modal').toggle();
+                })
+                 $('.modal_close').click(function()
+                {
+
+                $('#modal').hide();
+                })
+       </script>
 			
 `);
